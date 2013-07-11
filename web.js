e6d82066558b0648add8c973e29115933cd8d585
Keji Xu <Kejixu@gmail.com>
2013-07-11 22:24:42 +0000

    node reads html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-
+var fs = require('fs');
+var infile = "index.html";
+var text = fs.readFile(infile).toString;
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(text);
 });
 
 var port = process.env.PORT || 5000;
