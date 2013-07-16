2bc3de48ee66daeff7c1657854c8d2438b0efe3d
Keji Xu <Kejixu@gmail.com>
2013-07-16 23:10:24 +0000

    changes

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 //var fs = require('fs');
 //var infile = "index.html";
-var text = fs.readFile(infile).toString('utf-8');
+//var text = fs.readFile(infile).toString('utf-8');
 app.get('/', function(request, response) {
   response.send('temp');
 });
